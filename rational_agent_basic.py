dbf0abd611c592d56dee298247d5e827a8207bed
Aryan Luthra <aryanluthra@berkeley.edu>
2020-05-21 19:58:50 +0100

    added template for DQN training

--- rational_agent_basic.py
+++ rational_agent_basic.py
@@ -22,7 +22,7 @@ def print_game_state(gameState, notebook=False):
 
 def preprocess_state_image(img):
     result = np.mean(img, axis=0)
-    new_size_pil = (4*result.shape[1]) // 5, (4*result.shape[0]) // 5
+    new_size_pil = (4 * result.shape[1]) // 5, (4 * result.shape[0]) // 5
     result = Image.fromarray(result)
     result = result.resize(new_size_pil, resample=Image.LANCZOS)
     result = np.array(result)
@@ -30,7 +30,9 @@ def preprocess_state_image(img):
     return result
 
 
-def main(notebook=False):
+def main_random(notebook=False):
+    # Step 1: Initialize game enviornment
+
     game = DoomGame()
     game.load_config("vizdoom/scenarios/basic.cfg")
     game.init()
@@ -55,5 +57,92 @@ def main(notebook=False):
         time.sleep(1)
 
 
+def rational_trainer():
+    # Step 1: Initialize game enviornment
+    game = DoomGame()
+    game.load_config("vizdoom/scenarios/basic.cfg")
+    game.init()
+
+    shoot = [0, 0, 1]
+    left = [1, 0, 0]
+    right = [0, 1, 0]
+    actions = [shoot, left, right]
+
+    # Step 2: Intitialize replay memory capacity
+    # TODO
+
+    # Step 3: Construct and initialize policy network with random weights or weights from previous training sessions
+    # TODO
+
+    # Step 3: Clone policy network to make target network
+    # TODO
+
+    # Step 4: Iterate over episodes
+    episodes = 5
+    epsilon = 0.05
+    time_step_ctr = 0
+
+    for i in range(episodes):
+
+        # Step 5: Initialize game to a starting state
+        game.new_episode()
+
+        while not game.is_episode_finished():
+            # Step 6: Select an action, either exploration or exploitation
+            initial_state = game.get_state()
+
+            if random.random() < epsilon:  # Exploration
+                action_todo = random.choice(actions)
+            else:
+                # TODO: Choose action via exploitation of neural net
+                action_todo = random.choice(actions)  # REPLACE THIS LINE with action with the largest Q value
+
+            # Step 7: Execute selected action in an emulator
+            reward_received = game.make_action(action_todo)
+            final_state = game.get_state()
+
+            experience = initial_state, action_todo, reward_received, final_state
+
+            # Step 8: Store experience in replay memory
+            # TODO
+
+            # Step 9: Sample random batch from replay memory
+            # TODO
+
+            # Step 10: Pre-process states from branch
+            # TODO: just use the preprocess method
+
+            # Step 11a: Pass states through the policy network and aquire output Q-values
+            # TODO
+
+            # Step 11b: Calculate target Q values. Pass successor states for each action through the target network
+            #           use bellman equation to calculate target value
+            # TODO
+
+            # Step 11c: Calculate MSE (or any other) Loss between output and target values
+            # TODO: use scipy loss functions, do not write ur own
+
+            # Step 12: Use gradient descent, or ADAM, to update weights along the policy network
+            # TODO: Dont write backprop urself, use the deep learning library (Keras or Pytorch)
+
+            # Step 13: Every x timesteps, the weights of the target network are updated
+            #          to be the weights of the policy network, small pertubations can be added
+            time_step_lim = 20
+            if time_step_ctr >= time_step_lim:
+                # TODO: Update target network to copy of policy network
+                time_step_ctr = 0
+
+            # update required values
+            time_step_ctr += 1
+            time.sleep(0.01)
+        print("Result:", game.get_total_reward())
+        time.sleep(1)
+
+    # Step 14: Save NN weights to a file so that it can later be read for testing the agent
+    # TODO: use torch or keras builtin save method, do not write ur own or use pickle
+    
+
+
+
 if __name__ == '__main__':
-    main(notebook=False)
+    main_random(notebook=False)

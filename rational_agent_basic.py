13692d9048709bf21032311c69d3f4566af8d84e
Aryan Luthra <aryanluthra@berkeley.edu>
2020-06-07 23:42:20 -0700

    removed unused libs

--- rational_agent_basic.py
+++ rational_agent_basic.py
@@ -8,9 +8,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
-from itertools import count
 import matplotlib.pyplot as plt
-import cv2 as cv
 import pickle
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
